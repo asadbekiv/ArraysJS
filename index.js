7a5c03acc3014d970123536f85e879663e81ee84
Asadbek-lang <asadbekjon8899@gmail.com>
2024-01-02 23:11:57 +0500

    array tasks in prosses

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+console.log('My name means that I am Lion or I was born in the middle of Summer {June,July,August}');
+
+
+let fruits = ["Apples", "Pear", "Orange"];
+
+// push a new value into the "copy"
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+
+// what's in fruits?
+console.log( fruits.length ); // Output will be 4 ;
+
+
+
