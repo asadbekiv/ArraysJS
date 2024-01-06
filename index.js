7b68baa9acad5733ef4cf5dd686537dca6bfdfaf
Asadbek-lang <asadbekjon8899@gmail.com>
2024-01-06 20:46:08 +0500

    some tasks is done

--- index.js
+++ index.js
@@ -12,5 +12,55 @@ shoppingCart.push("Banana");
 // what's in fruits?
 console.log( fruits.length ); // Output will be 4 ;
 
+// Second task.
+
+let styles=new Array('Jazz','Blues');
+
+styles.push('Rock-n-Roll'); // it push the value to the end of the array
+styles[Math.floor((styles.length-1)/2)]='Classics';  // it find of the middle value in the array
+styles.shift(); // it removes first element of the array
+styles.unshift('Rap','Reggae'); //  it adds those values begining of array
+
+console.log(styles);
+console.log(styles.length);
 
 
+// Third task
+
+let arr=['a','b'];
+
+arr.push(function(){
+    console.log((this));
+})
+
+arr[2]();
+
+
+// Fourth task
+
+
+function sumInput(){
+    
+    let storeArr=new Array();
+
+    while (true) {
+        let value=prompt('Enter a number please )',0);
+
+        if (value === "" || value === null || !isFinite(value)) break;
+        
+        storeArr.push(+value)
+        
+    }
+    
+
+    let sum = 0;
+    for (const number of storeArr) {
+        sum +=number
+        
+    }
+    return sum;
+}
+
+alert(sumInput())
+
+// Fifth task
