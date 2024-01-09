5c533d711fffdb634c9277b3e768ac8541e1fb1a
Asadbek-lang <asadbekjon8899@gmail.com>
2024-01-09 22:03:10 +0500

    some of them is commpleted

--- index.js
+++ index.js
@@ -61,6 +61,31 @@ function sumInput(){
     return sum;
 }
 
-alert(sumInput())
+// alert(sumInput())
 
 // Fifth task
+
+
+function getMaxSubSum(arr){
+    let maxSum=0
+
+    for (let i = 0; i < arr.length; i++) {
+        let sumFixedStart=0;
+        for (let j = 0; j < arr.length; j++) {
+            sumFixedStart += arr[j];
+            maxSum=Math.max(maxSum,sumFixedStart);
+            
+        }
+        
+    }
+
+    return maxSum;
+}
+
+console.log(getMaxSubSum([-1,2,3,-9]));
+console.log(getMaxSubSum([-1, 2, 3, -9, 11]));
+console.log(getMaxSubSum([-2, -1, 1, 2]) );
+console.log(getMaxSubSum([1, 2, 3]));
+console.log(getMaxSubSum([100, -9, 2, -3, 5]));
+
+console.log('Hi');
